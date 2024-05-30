6e51531f8408125918744ab293c4cc13503b638f
Liviu <liviumocanu20009@gmail.com>
2024-05-30 16:40:52 +0300

    Refactor Controller Authorization and Validation Filter

--- Streamphony.WebAPI/Controllers/AppBaseController.cs
+++ Streamphony.WebAPI/Controllers/AppBaseController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Streamphony.WebAPI.Controllers;
 
 [ApiController]
+[Authorize]
 public abstract class AppBaseController : ControllerBase
 {
 }
--- Streamphony.WebAPI/Controllers/SongController.cs
+++ Streamphony.WebAPI/Controllers/SongController.cs
@@ -15,7 +15,6 @@ public class SongController(IMediator mediator) : AppBaseController
     private readonly IMediator _mediator = mediator;
 
     [HttpPost]
-    [Authorize]
     [ValidateModel]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -29,7 +28,6 @@ public class SongController(IMediator mediator) : AppBaseController
     }
 
     [HttpPut]
-    [Authorize]
     [ValidateModel]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -43,6 +41,7 @@ public class SongController(IMediator mediator) : AppBaseController
     }
 
     [HttpGet]
+    [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<PaginatedResult<SongDto>>> GetAllSongs([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
     {
@@ -57,6 +56,7 @@ public class SongController(IMediator mediator) : AppBaseController
     }
 
     [HttpPost("filtered")]
+    [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<PaginatedResult<SongDto>>> GetAllSongsFiltered(PagedRequest pagedRequest)
@@ -66,6 +66,7 @@ public class SongController(IMediator mediator) : AppBaseController
     }
 
     [HttpGet("{id}")]
+    [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<SongDto>> GetSongById(Guid id)
@@ -75,7 +76,6 @@ public class SongController(IMediator mediator) : AppBaseController
     }
 
     [HttpDelete("{id}")]
-    [Authorize]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
